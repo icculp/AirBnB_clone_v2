dd3ea10c5f5c7dca00c6094d94ce8ebc89252f54
Ian Culp <icculp@gmail.com>
2020-08-18 01:17:49 +0000

    splitting strings broke format variables

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -12,25 +12,24 @@ env.hosts = ['34.73.244.83', '35.175.186.218']
 
 def do_deploy(archive_path):
     """ deploy archive given as input to /tmp on both web servers """
-    if os.path.exists("./" + archive_path):
-        put(archive_path, '/tmp/')
-        print('true')
+    if os.path.exists(archive_path):
+        put(archive_path, "/tmp/")
     else:
-        print('returning false')
-        print("./" + archive_path)
         return False
     plist = archive_path.split('/')
     arch_name = plist[-1]
     wo_ext = arch_name[:-4]
+    print(arch_name)
+    print(wo_ext)
 
     run("mkdir -p /data/web_static/releases/{}".format(wo_ext))
-    run("tar -xzf /tmp/{} -C /data/" +
-        "web_static/releases/{}/".format(arch_name, wo_ext))
+    run("tar -xzf /tmp/{} -C /data/".format(arch_name) +
+        "web_static/releases/{}/".format(wo_ext))
     run("rm /tmp/{}".format(arch_name))
-    run("mv /data/web_static/releases/{}/" +
-        "web_static/* /data/web_static/releases/{}/".format(wo_ext, wo_ext))
+    run("mv /data/web_static/releases/{}/".format(wo_ext) +
+        "web_static/* /data/web_static/releases/{}/".format(wo_ext))
     run("rm -rf /data/web_static/releases/{}/web_static".format(wo_ext))
     run("rm -rf /data/web_static/current")
-    run("ln -s /data/web_static/releases/{}/ " +
-        "/data/web_static/current".format(wo_ext))
+    run("ln -s /data/web_static/releases/{}/ ".format(wo_ext) +
+        "/data/web_static/current")
     return True
