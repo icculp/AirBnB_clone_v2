d137bcde30576b95b725bc4f93377502d5b8d2d5
Ian <ian.culp@holbertonschool.com>
2020-08-31 20:03:36 +0000

    2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''
+    Task 0
+'''
+from flask import Flask
+from web_flask import app
+
+
+@app.route('/', strict_slashes=False)
+def hello_hello_hbnb():
+    ''' Hello HBNB! '''
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_hbnb():
+    ''' Hello HBNB! '''
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def hello_c(text):
+    ''' Hello HBNB! '''
+    return "C {}".format(text.replace("_", " "))
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
