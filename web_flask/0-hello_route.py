335a93d1246aef0780658b337054d9ac3326e149
Ian Culp <icculp@gmail.com>
2020-09-01 20:25:32 +0000

    0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,6 +6,9 @@ from flask import Flask
 from web_flask import app
 
 
+app = Flask(__name__)
+
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     ''' Hello HBNB! '''
--- web_flask/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-""" instatiates a Flask object """
-from flask import Flask
-
-
-app = Flask(__name__)
